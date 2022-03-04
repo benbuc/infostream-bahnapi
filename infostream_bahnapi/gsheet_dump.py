9f357e6fb3c8fb6d97b63d984d70fb07584b4906
Benito Buchheim <github.benito.2017@buchheims.de>
2022-03-04 12:50:03 +0100

    dumping relevant data

--- infostream_bahnapi/gsheet_dump.py
+++ infostream_bahnapi/gsheet_dump.py
@@ -1,35 +1,52 @@
 import pygsheets
 import pandas as pd
 from infostream_bahnapi.get_arrivals import get_arrivals
+import datetime
+
+LOOKAHEAD = 24  # hours
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 # authorization
 gc = pygsheets.authorize(service_file="credentials/creds.json")
 
-column_mapping = [
-    ("Status", 2),
-    ("Date", 3),
-    ("Time", 4),
-    ("Type", 5),
-    ("Station", 6),
-    ("ID", 7),
-    ("Description / Legend", 10),
-]
+# column_mapping = [
+#    ("Status", 2),
+#    ("Date", 3),
+#    ("Time", 4),
+#    ("Type", 5),
+#    ("Station", 6),
+#    ("ID", 7),
+#    ("Description / Legend", 10),
+# ]
+
+station_mapping = {
+    "Berlin Hbf": "Hbf (Train)",
+    "Berlin Südkreuz": "Suedkreuz (Train)",
+    "Berlin Ostbahnhof": "Ostbahnhof (Train)",
+}
 
 # Create empty dataframe
 # df = pd.DataFrame(columns=[c[0] for c in column_mapping])
 arrival_table = []
 
-arrival_data = get_arrivals(8 * 60)
+arrival_data = get_arrivals(LOOKAHEAD * 60)
 all_arrivals = arrival_data["all_arrivals"]
 for station in all_arrivals:
     station_name = station["station_name"]
     arrivals = station["arrivals"]
     for arrival in arrivals:
+        scheduled = datetime.datetime.fromisoformat(arrival["scheduled"])
+        estimated = datetime.datetime.fromisoformat(arrival["estimated"])
         arrival_table.append(
             {
-                "Date": arrival["scheduled"],
-                "Station": station_name,
+                "Date": scheduled.strftime("%Y-%m-%d"),
+                "Time": scheduled.strftime("%H:%M:%S"),
                 "ID": arrival["name"],
+                "Type": "Train",
+                "Bahnhof": station_mapping[station_name],
+                "Delay": "true" if arrival["delay"] > 0 else "false",
+                "DelayDateTime": estimated.strftime(DATETIME_FORMAT),
+                "From": "Origin",
             }
         )
 
@@ -42,7 +59,8 @@ wks = sh[0]
 print(df)
 
 # for mapping in column_mapping:
-# wks.set_dataframe(df, (1, 1))
+wks.set_dataframe(df, (1, 1))
+wks.update_value("M1", datetime.datetime.now().strftime(DATETIME_FORMAT))
 
-for mapping in column_mapping:
-    wks.set_dataframe(df.filter([mapping[0]]), (1, mapping[1]))
+# for mapping in column_mapping:
+#    wks.set_dataframe(df.filter([mapping[0]]), (1, mapping[1]))
